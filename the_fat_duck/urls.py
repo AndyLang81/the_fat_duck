68a3a04b9ce51dbed3908e6497244a50a1fe08ea
AndyLang81 <Andylanghoff@gmail.com>
2025-05-19 21:58:40 +0200

    Add custom homepage and fix root route to load index.html correctly

--- the_fat_duck/urls.py
+++ the_fat_duck/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("", TemplateView.as_view(template_name="index.html"), name="home"),
     path('admin/', admin.site.urls),
     path('', include('booking.urls')),
     path("menu/", TemplateView.as_view(template_name="menu.html"), name="menu"),
