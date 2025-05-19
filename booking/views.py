b9068eebb83314c15a1ac0e290ee692e3a74d7ba
AndyLang81 <Andylanghoff@gmail.com>
2025-05-19 20:38:44 +0200

    Add check to prevent duplicate bookings by date and time

--- booking/views.py
+++ booking/views.py
@@ -7,12 +7,26 @@ from .forms import BookingForm
 from django.http import HttpResponseRedirect
 
 def book_table(request):
+    message = None
+
     if request.method == 'POST':
         form = BookingForm(request.POST)
         if form.is_valid():
-            form.save()
-            return render(request, 'booking_success.html')
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            guests = form.cleaned_data['guests']
+            date = form.cleaned_data['date']
+            time = form.cleaned_data['time']
+
+            # Checks if date/time is already booked
+            exists = Booking.objects.filter(date=date, time=time).exists()
+
+            if exists:
+                message = "Sorry, that time slot is already booked. Please choose another."
+            else:
+                form.save()
+                return render(request, 'booking_success.html')
     else:
         form = BookingForm()
-    
-    return render(request, 'book_table.html', {'form': form})
+
+    return render(request, 'book_table.html', {'form': form, 'message': message})
