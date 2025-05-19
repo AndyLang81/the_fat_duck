9bef7aad90c0cb000d27f661b3cc293c37727f60
AndyLang81 <Andylanghoff@gmail.com>
2025-05-19 21:47:43 +0200

    Add homepage with intro text, imagery, and route to root URL

--- the_fat_duck/urls.py
+++ the_fat_duck/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', include('booking.urls')),
     path("menu/", TemplateView.as_view(template_name="menu.html"), name="menu"),
     path("contact/", TemplateView.as_view(template_name="contact.html"), name="contact"),
+    path("about/", TemplateView.as_view(template_name="about.html"), name="about"),
+    path("", TemplateView.as_view(template_name="index.html"), name="home"),
+
 ]
