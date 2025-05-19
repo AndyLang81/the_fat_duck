fcb07f93862544d30e735713dd489a643286ba77
AndyLang81 <Andylanghoff@gmail.com>
2025-05-19 21:22:52 +0200

    Add static contact page with images and opening hours

--- the_fat_duck/urls.py
+++ the_fat_duck/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('booking.urls')),
     path("menu/", TemplateView.as_view(template_name="menu.html"), name="menu"),
+    path("contact/", TemplateView.as_view(template_name="contact.html"), name="contact"),
 ]
