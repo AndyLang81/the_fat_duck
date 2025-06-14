7ac3cef3b60495f532109b6bdcb54ee07d5e4313
AndyLang81 <Andylanghoff@gmail.com>
2025-06-14 12:13:35 +0200

    Clean up root URL routing: avoid conflicts and simplify layout

--- the_fat_duck/urls.py
+++ the_fat_duck/urls.py
@@ -3,12 +3,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="index.html"), name="home"),
     path('admin/', admin.site.urls),
+
+    # Booking app routes
     path('', include('booking.urls')),
+
+    # Static template pages
     path("menu/", TemplateView.as_view(template_name="menu.html"), name="menu"),
     path("contact/", TemplateView.as_view(template_name="contact.html"), name="contact"),
     path("about/", TemplateView.as_view(template_name="about.html"), name="about"),
-    path("", TemplateView.as_view(template_name="index.html"), name="home"),
-
 ]
