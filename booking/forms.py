504ffe8853d9363d0d7a69661dcd74afd2b131c7
AndyLang81 <Andylanghoff@gmail.com>
2025-06-14 17:36:46 +0200

    Use select dropdown for time slots (17:00–22:00 in 30m increments)

--- booking/forms.py
+++ booking/forms.py
@@ -1,14 +1,25 @@
+# booking/forms.py
+
+import datetime
 from django import forms
 from .models import Booking
 
+# Generate half-hour slots from 17:00 to 22:00
+TIME_SLOTS = []
+start = datetime.datetime(2000, 1, 1, 17, 0)
+end   = datetime.datetime(2000, 1, 1, 22, 0)
+delta = datetime.timedelta(minutes=30)
+
+while start <= end:
+    slot = start.time().strftime("%H:%M")
+    TIME_SLOTS.append((slot, slot))
+    start += delta
+
 class BookingForm(forms.ModelForm):
-    # Override the time field to enforce 24-hour format
-    time = forms.TimeField(
-        widget=forms.TimeInput(
-            attrs={'type': 'time'},
-            format='%H:%M'
-        ),
-        input_formats=['%H:%M']
+    # Override time field to use a select dropdown of allowed slots
+    time = forms.ChoiceField(
+        choices=TIME_SLOTS,
+        widget=forms.Select(attrs={'class': 'form-control'})
     )
 
     class Meta:
@@ -16,5 +27,5 @@ class BookingForm(forms.ModelForm):
         fields = ['name', 'email', 'guests', 'date', 'time']
         widgets = {
             'date': forms.DateInput(attrs={'type': 'date'}),
-            # 'time' is defined above to ensure 24-hour clock
+            # time uses the select above
         }
